b427fad29f2fd86244065c3d879a1c5236373ffe
David Marland <davidm@rland.me.uk>
2019-02-19 20:16:41 +0000

    Store the ship placeholder count

--- src/Components/Organisms/FleetShips/FleetShips.tsx
+++ src/Components/Organisms/FleetShips/FleetShips.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { IFleetShip } from "../../../Interfaces";
-import { COLOURS } from "../../../styles/colours";
 import { GRID } from "../../../styles/variables";
 import { ListUnstyled } from "../../Atoms/Lists/ListUnstyled/ListUnstyled";
 import { FleetShipItem } from "../FleetShipItem/FleetShipItem";
@@ -21,17 +20,35 @@ const MoreLink = styled.div`
   justify-content: center;
 `;
 
+const LOCAL_STORAGE_KEY = 'FLEET_SHIP_PLACEHOLDER_COUNT'
+
 export const FleetShips = ({ ships }: IProps) => {
+  const [placeholderCount, setPlaceHolderCount] = React.useState(1);
+
+  React.useEffect(() => {
+    let count = 1;
+    if (ships) {
+      count = ships.length;
+      window.localStorage.setItem(LOCAL_STORAGE_KEY, count);
+    } else {
+      const lsValue = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+      if (lsValue) {
+        count = parseInt(lsValue, 10);
+      }
+    }
+    if (placeholderCount !== count) {
+      setPlaceHolderCount(count);
+    }
+  }, [ships]);
+
   let shipRows;
   if (ships !== undefined) {
     shipRows = ships.map(ship => (
       <FleetShipItem key={ship.ship.id} fleetShip={ship} />
     ));
   } else {
-    shipRows = [
-      <FleetShipItem key="loading1" />,
-      <FleetShipItem key="loading2" />,
-    ];
+    shipRows = Array.from(Array(placeholderCount).keys())
+      .map(a => (<FleetShipItem key={`placeholder-${a}`} />));
   }
 
   return (
