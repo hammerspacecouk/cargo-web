bf238ac87e5b7dd1c794ac810764bd8f15b4c866
David Marland <davidm@rland.me.uk>
2019-11-06 21:51:26 +0000

    Viewport Fit to allow safe area?

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -35,6 +35,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" sizes="1024x1024"
                 href="/app-icons/1024x1024.png" />
