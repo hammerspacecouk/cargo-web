5899b3ed7bf17b41beaca415b6b1d32c53a2ef56
David Marland <davidm@rland.me.uk>
2021-01-23 16:13:34 +0000

    Add note about trial

--- src/components/Pages/LoginPage.tsx
+++ src/components/Pages/LoginPage.tsx
@@ -5,11 +5,17 @@ import { ILoginOptions } from "@src/interfaces";
 import { SimplePage } from "@src/components/Templates/SimplePage";
 import { PanelPage } from "@src/components/Templates/PanelPage";
 import { ParsedUrlQuery } from "querystring";
+import {H3} from "@src/components/Atoms/Heading";
 
 export const LoginPage = ({ loginOptions, query }: IProps) => {
   return (
     <SimplePage disablePlayButton>
       <PanelPage title="Login">
+          <H3 as="p">
+              Trial the first 15% of the game (several hours of play) for <strong>FREE</strong>.
+              After which you may decide to upgrade to the full game.
+              <br /><br />
+          </H3>
         <LoginForm loginOptions={loginOptions} redirect={String(query?.r)} messages={messageQueryString(query)} />
       </PanelPage>
     </SimplePage>
